17e7e2470a85904ac2beedbb32a0f0317205af68
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-12-11 10:55:53 +0530

    Update example_test.py

--- tests/example_test.py
+++ tests/example_test.py
@@ -1,12 +1,17 @@
 import unittest
-from my_project import add
+from app import add, subtract
 
-class TestAddition(unittest.TestCase):
-    def test_add_positive_numbers(self):
-        self.assertEqual(add(2, 3), 5)
+class TestApp(unittest.TestCase):
 
-    def test_add_negative_numbers(self):
-        self.assertEqual(add(-2, -3), -5)
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, -2), -3)
+        self.assertEqual(add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(2, 1), 1)
+        self.assertEqual(subtract(-2, -1), -1)
+        self.assertEqual(subtract(0, 0), 0)
 
 if _name_ == '_main_':
     unittest.main()
